c2be598cafd55f31fd3d559d627f5423f13d0e38
Victor <vctrd3@gmail.com>
2020-07-10 14:08:35 -0400

    refactor search with usestate hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({icon, title}) => {
     <nav className="navbar bg-primary">
       <h1>
         <i className={icon} />
-        {' '}{title}
+        {' '}<Link to='/'>{title}</Link>
       </h1>
       <ul>
         <li>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export class Search extends Component {
-  state={
-    text: ''
+const Search = ({clearUsers, showClear, searchUsers, setAlert}) => {
+  const [text, setText] = useState('');
+
+  const handleOnChange = (e) => {
+    setText(e.target.value)
   }
 
-  handleOnChange = (e) => {
-    this.setState({ [e.target.name]:e.target.value })
-  }
-
-  handleOnSubmit = e => {
+  const handleOnSubmit = e => {
     e.preventDefault()
-    if(this.state.text === ''){
-      this.props.setAlert('Please enter something', 'light')
+    if(text === ''){
+      setAlert('Please enter something', 'light')
     } else{
-      this.props.searchUsers(this.state.text)
-    this.setState({text:''})
+      searchUsers(text)
+    setText('')
     }
   }
-  
-
-  render() {
-    const {clearUsers, showClear} = this.props
-    return (
-      <div>
-        <form className="form" onSubmit={this.handleOnSubmit}>
-          <input type="text" name="text" value={this.state.text} placeholder="Search user"
-          onChange={this.handleOnChange}
-          />
-          <input type="submit" value="search" className="btn btn-dark btn-block" />
-        </form>
-        {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
-        {/* <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button> */}
-      </div>
+    
+  return (
+    <div>
+      <form className="form" onSubmit={handleOnSubmit}>
+        <input type="text" name="text" value={text} placeholder="Search user"
+        onChange={handleOnChange}
+        />
+        <input type="submit" value="search" className="btn btn-dark btn-block" />
+      </form>
+      {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+      
+    </div>
     )
   }
-}
+
 
 export default Search
